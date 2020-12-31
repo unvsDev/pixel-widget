4fbe86c9bd8421ef4645edf6920e695e77522451
Minseo Kang <63099769+unvsDev@users.noreply.github.com>
2020-12-31 18:52:21 +0900

    Pixel Launcher v2.3 Release

--- Pixel Launcher.js
+++ Pixel Launcher.js
@@ -1,24 +1,26 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-blue; icon-glyph: cannabis;
-// Pixel Launcher v2.2 - by unvsDev
+// Pixel Launcher v2.3 - by unvsDev
 // for Setting up Pixel Widget
 
 // Unauthorized Redistribute is Strictly prohibited.
 // Contact developer for question, or reporting abuse
 // You can use Discord to contact @unvsDev!
 
-const version = 2.2
+const version = 2.3
 const plName = Script.name()
 
 var fm = FileManager.iCloud()
-var filePath = "/var/mobile/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/";
-var prefPath = fm.joinPath(filePath, "pixelPref.txt")
-var namePath = fm.joinPath(filePath, "plName.txt")
+var fDir = fm.joinPath(fm.documentsDirectory(), "/Pixel Widget")
+if(!fm.fileExists(fDir)){ fm.createDirectory(fDir) }
+var prefPath = fm.joinPath(fDir, "pixelPref.txt")
+var namePath = fm.joinPath(fDir, "plName.txt")
+var progPath = fm.joinPath(fDir, "plPlugin.txt")
 
-fm.writeString(namePath, plName)
+fm.writeString(namePath, JSON.stringify({"name":plName, "update":"false"}))
 
-var defaultJSON = {"apikey":"openweatherapikey","cityid":"1835848","layout":"pixel","username":"Sir","tempunit":"metric","locale":"en","textcolor":"#ffffff","textsize":"23","iconcolor":"default","iconsize":"27","font":"Product Sans","fontbold":"Product Sans Medium","spacing":"45","previewmode":"true","previewsize":"medium","refreshview":"false","greeting1":"Good morning","greeting2":"Good afternoon","greeting3":"Good evening","greeting4":"Good night","greeting5":"Time to sleep","greeting0":"Welcome to Pixel Widget","dateformat":"MMMM, EEE dd","quotemode":"false","bgmode":"solid","bglight":"null","bgdark":"null","bgcolor":"#147158","iconrefresh":"true","refreshrate":"90","ddaymode":"false","ddayname":"Christmas","ddaytarg":"2021-12-25","hideb":"false","event":"true"}
+var defaultJSON = {"apikey":"openweatherapikey","cityid":"1835848","layout":"pixel","username":"Sir","tempunit":"metric","locale":"en","textcolor":"#ffffff","textsize":"23","iconcolor":"default","iconsize":"27","font":"Product Sans","fontbold":"Product Sans Medium","spacing":"45","previewmode":"true","previewsize":"medium","refreshview":"false","greeting1":"Good morning","greeting2":"Good afternoon","greeting3":"Good evening","greeting4":"Good night","greeting5":"Time to sleep","greeting0":"Welcome to Pixel Widget","dateformat":"MMMM, EEE dd","quotemode":"false","bgmode":"solid","bglight":"null","bgdark":"null","bgcolor":"#147158","iconrefresh":"true","wallrefresh":"true","refreshrate":"90","ddaymode":"false","ddayname":"Christmas","ddaytarg":"2021-12-25","hideb":"false","event":"true"}
 
 var optionName = {
   "apikey": "Openweather API Key",
@@ -44,12 +46,13 @@ var optionName = {
   "greeting5": "Sleep Greeting",
   "greeting0": "Fixed Greeting",
   "dateformat": "Date Format",
-  "quotemode": "UseFixedGreeting",
+  "quotemode": "Use Fixed Greeting",
   "bgmode": "Background Mode",
   "bglight": "Select Light Background",
   "bgdark": "Select Dark Background",
   "bgcolor": "Background Color",
-  "iconrefresh": "AlwaysRefreshWeatherIcon",
+  "iconrefresh": "Always Refresh Weather Icon",
+  "wallrefresh": "Always Refresh Wallpaper",
   "refreshrate": "Widget Refresh Rate",
   "ddaymode": "Day Count Mode",
   "ddayname": "Day Count Name",
@@ -88,6 +91,7 @@ var optionFormat = {
   "bgdark": "for auto mode",
   "bgcolor": "(Color Hex Code) - solid mode",
   "iconrefresh": "(true, false) - recommended option",
+  "wallrefresh": "(true, false) - recommended option",
   "refreshrate": "(second)",
   "ddaymode": "(true, false)",
   "ddayname": "(Name)",
@@ -99,25 +103,132 @@ var optionFormat = {
 var welcomemode = 0
 
 // Preparing File
-let prefData0; var settingmode = 0;
+let prefData0; var settingmode = 0; var pluginmode = 0;
 if(!(fm.fileExists(prefPath))) {
   welcomemode = 1
   await fm.writeString(prefPath, JSON.stringify(defaultJSON))
 }
 
+var orgProgData = {
+    "covidkr1": false,
+    "covidkr2": "Hide",
+    "minimemo": "",
+    "minidday": ["", ""]
+}
+let progData
+if(!(fm.fileExists(progPath))) {
+  progData = orgProgData
+  await fm.writeString(progPath, JSON.stringify(progData))
+}
+
+// Main Menu
 let plAlert = new Alert()
 plAlert.title = welcomemode ? "Welcome to Pixel Widget!" : "Pixel Launcher"
-let menuOptions = ["Edit Preferences", "General Settings"]
+let menuOptions = ["Edit Preferences", "Manage Plugin", "General Settings"]
 for(const option of menuOptions) {
   plAlert.addAction(option)
 }
 plAlert.addCancelAction("Done")
 let response = await plAlert.presentAlert()
 if(response == 1){
-  settingmode = 1
+  pluginmode = 1
 } else if(response == 2) {
-  
+  settingmode = 1
 } else if(response == -1) {
+  return 0
+}
+
+
+if(pluginmode){
+  progData = JSON.parse(fm.readString(progPath))
+  
+  // Auto Update Preferences
+  var cnt = 0
+  for(i in orgProgData){
+    if(progData[i] == undefined){
+      cnt = cnt + 1
+      progData[i] = orgProgData[i]
+      console.log("[!] Updating preferences.. (" + cnt + ")")
+    }
+  }
+  
+  let pluginMenu = new UITable()
+  pluginMenu.showSeparators = true
+  
+  const covidkr1 = new UITableRow()
+  covidkr1.dismissOnSelect = false
+  covidkr1.addText("Covid-19 KR Information", "코로나19 확진자 정보를 간단히 알려줍니다. (KR Local)")
+  pluginMenu.addRow(covidkr1)
+  
+  covidkr1.onSelect = async () => {
+    let pAlert = new Alert()
+    pAlert.title = "Edit Covid-19 KR Information"
+    pAlert.message = "Currently set to \"" + (progData.covidkr1 ? "Show" : "Hide") + "\""
+    pAlert.addAction("Show")
+    pAlert.addAction("Hide")
+    progData.covidkr1 = (await pAlert.present() ? false : true)
+  }
+  
+  const covidkr2 = new UITableRow()
+  covidkr2.dismissOnSelect = false
+  covidkr2.addText("QR Check-in Shortcut", "QR 체크인 바로가기를 보여줍니다. (KR Local)")
+  pluginMenu.addRow(covidkr2)
+  
+  covidkr2.onSelect = async () => {
+    let pAlert = new Alert()
+    pAlert.title = "Edit QR Check-in Shortcut"
+    pAlert.message = "Currently set to \"" + progData.covidkr2 + "\""
+    pAlert.addAction("Show Naver")
+    pAlert.addAction("Show Kakao")
+    pAlert.addAction("Hide")
+    let response = await pAlert.present()
+    if(response == 0) { progData.covidkr2 = "Naver" }
+    else if(response == 1) { progData.covidkr2 = "Kakao" }
+    else { progData.covidkr2 = "Hide" }
+  }
+  
+  const minimemo = new UITableRow()
+  minimemo.dismissOnSelect = false
+  minimemo.addText("Mini Memo", "Type your quote, or short memo!")
+  pluginMenu.addRow(minimemo)
+  
+  minimemo.onSelect = async () => {
+    let pAlert = new Alert()
+    pAlert.title = "Edit Mini Memo"
+    pAlert.message = (progData.minimemo.length < 1 ? "Mini Memo is turned off currently." : "Mini Memo is set to \"" + progData.minimemo + "\"") + "\nLeave it blank to set to hide."
+    pAlert.addTextField("Type Memo", progData.minimemo)
+    pAlert.addCancelAction("Cancel")
+    pAlert.addAction("Done")
+    let response = await pAlert.present()
+    if(response != -1) {
+      progData.minimemo = pAlert.textFieldValue()
+    }
+  }
+  
+  const minidday = new UITableRow()
+  minidday.dismissOnSelect = false
+  minidday.addText("Mini Day Counter", "Celebrate your special day!")
+  pluginMenu.addRow(minidday)
+  
+  minidday.onSelect = async () => {
+    let pAlert = new Alert()
+    pAlert.title = "Edit Mini Day Counter"
+    pAlert.message = (progData.minidday[0].length < 1 ? "Day counter is turned off currently." : progData.minidday[1] + " is set to " + progData.minidday[0]) + "\nLeave it blank to set to hide, or input Target Date."
+    pAlert.addTextField("YYYY-MM-DD", progData.minidday[0])
+    pAlert.addTextField("Day Count Name", progData.minidday[1])
+    pAlert.addCancelAction("Cancel")
+    pAlert.addAction("Done")
+    let response = await pAlert.present()
+    if(response != -1) {
+      progData.minidday[0] = pAlert.textFieldValue(0)
+      progData.minidday[1] = pAlert.textFieldValue(1)
+    }
+  }
+  
+  await pluginMenu.present()
+  
+  await fm.writeString(progPath, JSON.stringify(progData))
+  
   return 0
 }
 
@@ -162,7 +273,7 @@ if(settingmode){
   settings.addRow(option3)
   
   option3.onSelect = async () => {
-    fm.writeString(namePath, "forceupdate")
+    fm.writeString(namePath, JSON.stringify({"name":plName, "update":"true"}))
     let fuAlert = new Alert()
     fuAlert.title = "Requested Update"
     fuAlert.message = "Launch Pixel Widget to begin Update. Your Preferences won't be deleted."
@@ -223,6 +334,7 @@ var prevData = {
   "bgdark": prefData.bgdark,
   "bgcolor": prefData.bgcolor,
   "iconrefresh": prefData.iconrefresh,
+  "wallrefresh": prefData.wallrefresh,
   "refreshrate": prefData.refreshrate,
   "ddaymode": prefData.ddaymode,
   "ddayname": prefData.ddayname,
@@ -253,7 +365,7 @@ for(title in prevData){
   option.addText(optionName[title], optionFormat[title])
   settings.addRow(option)
   option.onSelect = async (number) => {
-    if(number != 25 && number != 26){
+    if(number != 24 && number != 25 && number != 26){
       let editAlert = new Alert()
       editAlert.title = "Edit " + optionName[optionList[number]]
       editAlert.addTextField(optionFormat[optionList[number]], prevData[optionList[number]].toString())
@@ -262,21 +374,51 @@ for(title in prevData){
       if(await editAlert.present() == 0){
         prevData[optionList[number]] = editAlert.textFieldValue()
       }
-    }
-    else if(number == 25){
-      prevData.bglight = await DocumentPicker.openFile()
+    } else if(number == 24){
       let bgPickerAlert = new Alert()
-      bgPickerAlert.title = "Light Wallpaper"
-      bgPickerAlert.message = "Successfully saved your wallpaper!"
-      bgPickerAlert.addAction("OK")
-      await bgPickerAlert.present()
+      bgPickerAlert.title = "Edit Wallpaper Mode"
+      bgPickerAlert.message = "Currently set to " + prevData.bgmode + "."
+      bgPickerAlert.addAction("Fixed - One wallpaper")
+      bgPickerAlert.addAction("Auto - Two wallpaper")
+      bgPickerAlert.addAction("Solid - Simple color")
+      bgPickerAlert.addAction("Gradient - Based on current time")
+      bgPickerAlert.addCancelAction("Cancel")
+      let response = await bgPickerAlert.present()
+      if(response == 0) { prevData.bgmode = "fixed" }
+      else if(response == 1) { prevData.bgmode = "auto" }
+      else if(response == 2) { prevData.bgmode = "solid" }
+      else if(response == 3) { prevData.bgmode = "gradient" }
+      
+    } else if(number == 25){
+      if(prevData.bgmode == "fixed" || prevData.bgmode == "auto") {
+        prevData.bglight = await DocumentPicker.openFile()
+        let bgPickerAlert = new Alert()
+        bgPickerAlert.title = "Light Wallpaper"
+        bgPickerAlert.message = "Successfully saved your wallpaper!"
+        bgPickerAlert.addAction("OK")
+        await bgPickerAlert.present()
+      } else {
+        let bgPickerAlert = new Alert()
+        bgPickerAlert.title = "Light Wallpaper"
+        bgPickerAlert.message = "You can edit this area\nif you're in fixed / auto mode."
+        bgPickerAlert.addAction("OK")
+        await bgPickerAlert.present()
+      }
     } else if(number == 26){
-      prevData.bgdark = await DocumentPicker.openFile()
-      let bgPickerAlert = new Alert()
-      bgPickerAlert.title = "Dark Wallpaper"
-      bgPickerAlert.message = "Successfully saved your wallpaper!"
-      bgPickerAlert.addAction("OK")
-      await bgPickerAlert.present()
+      if(prevData.bgmode == "auto") {
+        prevData.bgdark = await DocumentPicker.openFile()
+        let bgPickerAlert = new Alert()
+        bgPickerAlert.title = "Dark Wallpaper"
+        bgPickerAlert.message = "Successfully saved your wallpaper!"
+        bgPickerAlert.addAction("OK")
+        await bgPickerAlert.present()
+      } else {
+        let bgPickerAlert = new Alert()
+        bgPickerAlert.title = "Dark Wallpaper"
+        bgPickerAlert.message = "You can edit this area if you're in auto mode."
+        bgPickerAlert.addAction("OK")
+        await bgPickerAlert.present()
+      }
     }
   }
 }
